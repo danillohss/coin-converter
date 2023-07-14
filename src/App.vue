4203d64a79c4ef330a71deac2cf960d6aa65eed8
Danillo Soares <danillohss@gmail.com>
2023-07-14 03:24:16 -0300

    Conversor de moedas

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Converter coinA="USD" coinB="BRL" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Converter from "@/components/Converter.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Converter },
+};
 </script>
 
 <style>
---
+++ src/components/Converter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="converter">
+    <h2>{{ coinA }} > {{ coinB }}</h2>
+    <input v-model="coinA_value" type="text" :placeholder="coinA" />
+    <button @click="converter">Converter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CONVERTER",
+  props: ["coinA", "coinB"],
+  data() {
+    return {
+      coinA_value: "",
+      coinB_value: "",
+    };
+  },
+  methods: {
+    converter() {
+      let url = `http://free.currencyconverterapi.com/api/v5/convert?q=${this.coinA}_${this.coinB}&compact=y`;
+      fetch(url);
+    },
+  },
+};
+</script>
